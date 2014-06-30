d6c42647c62dc319fe7d2d15e10b9a8368caa5ef
jksfotemp <josephkelly.resume@gmail.com>
2014-06-30 02:09:46 +0000

    Changed to longer video, stopping for night

--- js/default.11.js
+++ js/default.11.js
@@ -2,12 +2,19 @@
 // window.onload=alert('in js file');
 console.log('in js file');
 
+var barSize; 
+var myMovie; 
+var playButton;
+var defaultBar;
+var progressBar;
+
+
 function doFirst(){
-    var barSize=600; 
-    var myMovie = document.getElementById ('myMovie');
-    var playButton = document.getElementById ('playButton');
-    var defaultBar = document.getElementById ('defaultBar');
-    var progressBar = document.getElementById ('progressBar');
+    barSize=600; 
+    myMovie = document.getElementById ('myMovie');
+    playButton = document.getElementById ('playButton');
+    defaultBar = document.getElementById ('defaultBar');
+    progressBar = document.getElementById ('progressBar');
 
     playButton.addEventListener('click', playOrPause, false);
     defaultBar.addEventListener('click', clickedBar, false);
@@ -17,22 +24,38 @@ function doFirst(){
 
 function playOrPause (){
 
+    
     if (!myMovie.paused && !myMovie.ended){
         myMovie.pause();
         playButton.innerHTML="Play";
         window.clearInterval(updateBar);
-        
     }
     else{
         myMovie.play();
-        playButton.innerHTML="Pause"
+        playButton.innerHTML="Pause";
+        var updateBar = setInterval (update, 500); 
     }
-
-
-    ;
 }
 
-function clickedBar (){
-    ;
+function update (){
+    if (!myMovie.ended){
+        var size=parseInt((myMovie.currentTime * barSize/myMovie).duration); 
+        progressBar.style.width = size + 'px';
+    }
+    else {
+        progressBar.style.width = '0px';
+        playButton.innerHTML="Play";
+        window.clearInterval();
+    }
 }
 
+function clickedBar (e){
+    if (!myMovie.paused && !myMovie.ended) {
+        var mouseX = e.pageX - defaultBar.offsetLeft;
+        var newTime = (mouseX * myMovie.duration)/barSize;
+        myMovie.currentTime = newTime;
+        progressBar.style.width = mouseX + 'px';
+    }
+}
+
+window.addEventListener ('load', doFirst, false); 
